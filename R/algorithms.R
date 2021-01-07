a07e34a4e7048f127b068171b4c9a07bf55f38d7
christinening <51478989+christinening@users.noreply.github.com>
2021-01-07 08:04:31 +0100

    add get_user_rank function

--- R/algorithms.R
+++ R/algorithms.R
@@ -1,4 +1,5 @@
 #'Get Alpha Centrality
+#'
 #'@param A Square adjacency matrix
 #'@param a penalty term
 #'
@@ -42,7 +43,8 @@ get_alphas <- function(A, a, path = NULL) {
 }
 
 #'Dynamic Communicability Matrix (dcm)
-#'Input list of alpha centralities created using the function store_alpha(...)
+#'
+#'Input list of alpha centralities created using the function \code{\link{store_alpha}}
 #'and calculates dynamic communicability matrices as described in "A matrix
 #'iteration for dynamic network summaries" by Grindrod, P. and Higham.
 #'
@@ -68,12 +70,14 @@ dcm_simple <- function(B) {
 
 
 #'Running Dynamic Communicability Matrix (RDCM)
-#'Input list of alpha centralities created using the function store_alpha(...)
+#'
+#'Input list of alpha centralities created using the function \code{\link{store_alpha}}
 #'and parameter b to calculate running dynamic communicability matrices as described
 #'in "A matrix iteration for dynamic network summaries" by Grindrod, P. and Higham.
 #'
 #'@param B List of alpha centrality matrices
 #'@param b parameter for downweighting older walks
+#'@param dt vector of time differences between two timepoints
 #'@return List of running dynamic communicability matrices
 
 rdcm_simple <- function(B, b, dt) {
@@ -89,21 +93,24 @@ rdcm_simple <- function(B, b, dt) {
   S <- vector("list", k)
   S[[1]] <- B[[1]]-Unit
   for(i in 2:k){
+    begin <- Sys.time()
     S[[i]] <- ((Unit+exp(-b*dt[i-1])*S[[i-1]]) %*% B[[i]]) - Unit
+    cat("\rRDCM ", i, "/", length(B), " done in ",
+        round(Sys.time() - begin, 1), ".")
   }
   return(S)
 }
 
+#'Find upper bound for alpha
+#'
 #'Input list of adjacency matrices to calculate the upper bound of parameter a
-#'through the spectral radius of the adjacency matrices
-#'
+#'through the spectral radius of the adjacency matrices.
 #'WARNING: can be REALLY slow!!!
-#'may not be recommended at all, if matrix is too large or eigenvalue can not be found
-#'
-#'an upper bound of largest eigenvalue should be used instead
-#'CN: hard to find one for our case: non-negative and asymmetric matrix (+non-regular graph)
+#'May not be recommended at all, if matrix is too large or eigenvalue can not be found.
+#'An upper bound of largest eigenvalue or spectral radius should be used instead
+#'hard to find one for our case: non-negative and asymmetric matrix (+non-regular graph)
 #'used Theorem 2.1 from https://www.sciencedirect.com/science/article/pii/S0024379513005405
-#'calculating an upper bound of spectral radius
+#'calculating an upper bound of spectral radius.
 #'
 #'@param A list of adjacency matrices
 #'@param exact boolean, if TRUE the exact eigenvalues are calculated, else only the upper
@@ -123,8 +130,8 @@ get_upperbound_a <- function(A, exact = FALSE){
     spectral_radius <- numeric(length(A))
 
     for(i in 1:length(A)){
-      cat("\rcalc upper bound for spectral radius of A[", i, "]")
-      tmp_rs <- sort(rowSums(A[[i]]), decreasing = TRUE)
+      cat("\rcalc upper bound for spectral radius of A[", i, "]\n")
+      tmp_rs <- sort(Matrix::rowSums(A[[i]]), decreasing = TRUE)
       tmp_phi <- numeric(length(tmp_rs))
       ### since we have an adjacency matrix without self-loops, N = 1, M = 0
       for(j in 1:length(tmp_rs)){
@@ -135,4 +142,36 @@ get_upperbound_a <- function(A, exact = FALSE){
     max_spectralradius <- min(spectral_radius)
   }
   return(1/max_spectralradius)
+}
+
+#'Rank users
+#'
+#'Return ranked users regarding the dynamic broadcast or receive communicabilities
+#'for each time point
+#'Input list of running dynamic communicability matrices (an outcome of the function
+#'\code{\link{rdcm_simple}})
+#'
+#'@param rdcm List of running dynamic communicability matrices
+#'@param type either "broadcast" or "receive"
+#'@param mappings vector of mapping between "ID" and "Node" (an outcome of the function
+#'\code{\link{create_adj_mat_time}})
+#'@param tiesmethod method to rank when ties occur
+#'@return matrix of user rank for each time point
+get_user_rank <- function(rdcm,
+                          type = "broadcast",
+                          mappings,
+                          tiesmethod = "average"){
+
+  users_rank <- data.table(names(mapping))
+
+  for(i in 1:length(rdcm)){
+    if(type == "broadcast"){
+      tmp_S <- Matrix::colSums(rdcm[[i]])
+    } else if(type == "receive"){
+      tmp_S <- Matrix::rowSums(rdcm[[i]])
+    }
+    users_rank <- cbind(users_rank, tmp_S, frank(-tmp_S, ties.method = tiesmethod))
+  }
+  colnames(users_rank) <- c("User_ID", paste0(c("value_t", "rank_t"), rep(1:length(rdcm), each = 2)))
+  return(users_rank)
 }
